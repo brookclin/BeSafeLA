19426d3cdfa1958f8aab2e9c6ff4628cc7a64cc1
dawnsmile <dawnsmilesu@gmail.com>
2017-11-28 12:24:50 -0800

    update infowindow

--- route.js
+++ route.js
@@ -12,7 +12,7 @@ function initMap() {
         zoom: 15,
         center: { lat: 34.02255525, lng: -118.28189665 }
     });
-// https://maps.google.com/maps/api/staticmap?center=34.02255525%2C-118.28189665&zoom=15&size=256x256&language=en&sensor=false&client=google-maps-frontend&signature=reL4qZBZ1BTUXSsvk7E0X8k1y4Q
+    // https://maps.google.com/maps/api/staticmap?center=34.02255525%2C-118.28189665&zoom=15&size=256x256&language=en&sensor=false&client=google-maps-frontend&signature=reL4qZBZ1BTUXSsvk7E0X8k1y4Q
     map.data.loadGeoJson('lapd-bureaus.geojson');
     map.data.setStyle({ fillColor: '#007bff', fillOpacity: 0.01, strokeWeight: 1.2, strokeColor: '#007bff', strokeOpacity: 0.5 });
 
@@ -20,17 +20,17 @@ function initMap() {
 
 
     map.addListener('mouseout', function() {
-        if (directionsDisplay.map==null){
-            if (document.getElementById('startButton')['disabled']){
-            document.getElementById('startButton')['disabled']=null;    
+        if (directionsDisplay.map == null) {
+            if (document.getElementById('startButton')['disabled']) {
+                document.getElementById('startButton')['disabled'] = null;
+            }
+
+            if (document.getElementById('endButton')['disabled']) {
+                document.getElementById('endButton')['disabled'] = null;
+            }
         }
 
-        if (document.getElementById('endButton')['disabled']){
-            document.getElementById('endButton')['disabled']=null;    
-        }
-        }
-        
-         
+
         if (startListener) {
             google.maps.event.removeListener(startListener);
         }
@@ -39,11 +39,6 @@ function initMap() {
         }
     });
 
-    // var onChangeHandler = function() {
-    //     calculateAndDisplayRoute(directionsService, directionsDisplay);
-    // };
-    // document.getElementById('start').addEventListener('change', onChangeHandler);
-    // document.getElementById('end').addEventListener('change', onChangeHandler);
 
     google.maps.event.addDomListener(document.getElementById('startButton'), 'click', function() {
 
@@ -52,9 +47,7 @@ function initMap() {
             crimeMarkerArray[i].setMap(null);
         }
         crimeMarkerArray = [];
-        document.getElementById('startButton')["disabled"]="disabled";        
-        // document.getElementById('startAddress').innerHTML = "start point address";
-        // document.getElementById('endAddress').innerHTML = "end point address";
+        document.getElementById('startButton')["disabled"] = "disabled";
 
         startListener = map.data.addListener('click', function(e) {
             if (startMarker) {
@@ -95,7 +88,7 @@ function initMap() {
             crimeMarkerArray[i].setMap(null);
         }
         crimeMarkerArray = [];
-        document.getElementById('endButton')["disabled"]="disabled";  
+        document.getElementById('endButton')["disabled"] = "disabled";
         // document.getElementById('startAddress').innerHTML = "start point address";
         // document.getElementById('endAddress').innerHTML = "end point address";
         endListener = map.data.addListener('click', function(e) {
@@ -131,8 +124,8 @@ function initMap() {
     });
 
     google.maps.event.addDomListener(document.getElementById('find'), 'click', function() {
-        document.getElementById('endButton')["disabled"]="disabled";  
-                    document.getElementById('startButton')["disabled"]="disabled";  
+        document.getElementById('endButton')["disabled"] = "disabled";
+        document.getElementById('startButton')["disabled"] = "disabled";
         directionsService.route({
             origin: startMarker.position,
             destination: endMarker.position,
@@ -163,10 +156,16 @@ function initMap() {
                                         clng = +a.split(',')[1];
                                         if (Math.abs(clat - lat) < 0.002 && Math.abs(clng - lng) < 0.002) {
 
-                                            if (crimeLocations['' + clat +' '+ clng]) {
-                                                crimeLocations['' + clat +' '+ clng] += 1
+                                            if (crimeLocations['' + clat + ' ' + clng]) {
+                                                // crimeLocations['' + clat + ' ' + clng].count+=1
+                                                if (crimeLocations['' + clat + ' ' + clng][data[crimeidx]['Crime Code']]) {
+                                                    crimeLocations['' + clat + ' ' + clng][data[crimeidx]['Crime Code']]['count'] += 1
+                                                } else {
+                                                    crimeLocations['' + clat + ' ' + clng][data[crimeidx]['Crime Code']] = { 'description': data[crimeidx]['Crime Code Description'], 'count': 1 }
+                                                }
                                             } else {
-                                                crimeLocations['' + clat +' '+ clng] = 1
+                                                crimeLocations['' + clat + ' ' + clng] = {}
+                                                crimeLocations['' + clat + ' ' + clng][data[crimeidx]['Crime Code']] = { 'description': data[crimeidx]['Crime Code Description'], 'count': 1 }
                                             }
                                             // console.log(data[crimeidx]['Date Occurred']);
 
@@ -206,6 +205,10 @@ function initMap() {
                     }
 
                     for (var key in crimeLocations) {
+                        var totalcount=0;
+                        for (var i=0;i<Object.values(crimeLocations[key]).length;i++){
+                            totalcount+=Object.values(crimeLocations[key])[i].count;
+                        }
                         var crimeMarker = new google.maps.Marker({
                             position: { lat: +key.split(' ')[0], lng: +key.split(' ')[1] },
                             map: map,
@@ -216,24 +219,24 @@ function initMap() {
                                 strokeColor: '#007bff',
                                 strokeOpacity: 0,
                                 strokeWeight: 1,
-                                fillOpacity: 0.02*crimeLocations[key]>0.8?0.8:0.02*crimeLocations[key]
+                                fillOpacity: 0.02*totalcount>0.8?0.8:0.02*totalcount
 
                             }
                         });
 
-                        setCrimeMarker(crimeMarker, crimeLocations[key]);
+                        setCrimeMarker(crimeMarker, Object.values(crimeLocations[key]), crimeMarkerArray.length);
                         crimeMarkerArray.push(crimeMarker);
 
                     }
-                    
-                    console.log(Object.values(crimeStatics));
+
+                    // console.log(Object.values(crimeStatics));
                 });
 
 
 
                 directionsDisplay.setDirections(response);
                 directionsDisplay.setMap(map);
-                console.log(directionsDisplay);
+                // console.log(directionsDisplay);
 
 
 
@@ -259,21 +262,91 @@ function initMap() {
         // endMarker.setMap(null);
         startMarker = null;
         endMarker = null;
-        document.getElementById('endButton')["disabled"]=null;  
-                    document.getElementById('startButton')["disabled"]=null;  
+        document.getElementById('endButton')["disabled"] = null;
+        document.getElementById('startButton')["disabled"] = null;
     });
 }
 
-function setCrimeMarker(marker, count) {
-    var crimeInfoWindow = new google.maps.InfoWindow({ content: 'Crime Count:'+count });
-    marker.addListener('mouseover', function() {
+var margin = { top: 15, right: 25, bottom: 15, left: 0  }
+
+function setCrimeMarker(marker, data, markeridx) {
+    var crimeInfoWindow = new google.maps.InfoWindow();
+    var container = d3.select(document.createElement("div"));
+
+    var width = 200 - margin.left - margin.right;
+    var height = 200 - margin.top - margin.bottom;
+    var svg = container.append('svg').attr('width', width + margin.left + margin.right)
+        .attr('height', width + margin.top + margin.bottom)
+        .append('g')
+        .attr("transform","translate("+margin.left+","+margin.top+")");
+    // svg.selectAll('text').data(data).enter().append('text')
+    //     .text(function(d){return d.description.split(" ")[0]+d.count;});
+    var x=d3.scaleLinear()
+        .range([0,width])
+        .domain([0,d3.max(data,function(d){return d.count;})]);
+    var y=d3.scaleBand()
+        .rangeRound([height,0])
+        .padding(0.5)
+        .domain(data.map(function(d){
+            return d.description;
+        }));
+    var yAxis=d3.axisLeft(y).ticks(0);
+    var gy=svg.append("g")
+            .attr("class","yaxis")
+            .call(yAxis);
+    var bars=svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g");
+    bars.append("rect")
+        .attr("class","bar")
+        .attr("y",function(d){
+            return y(d.description);
+        })
+        .attr("height",y.bandwidth())
+        .attr("x",0)
+        .attr("width",function(d){
+            return x(d.count);
+        })
+        .attr('fill','#007bff')
+        .attr('fill-opacity',0.7);
+    var texts = svg.selectAll(".label")
+            .data(data);
+    texts.text(function (d) {
+            return d.description.split(" ")[0];;
+        });
+    texts.enter()
+            .append("text")
+            .attr("class", "label")
+            .attr("y", function (d) {
+                return y(d.description) + y.bandwidth() / 2+5;
+            })
+            .style("font-size", "1em")
+            .style("fill", "black")
+            //x position is 3 pixels to the right of the bar
+            .attr("x", 3)
+            .text(function (d) {
+                return d.description.split(" ")[0]+":"+d.count;
+            });
+
+    
+    var graphhtml = container.node().outerHTML;
+    crimeInfoWindow.setContent(graphhtml);
+
+    marker.addListener('click', function() {
         // console.log(data[crimeidx]);
+        //     var svg=d3.select('#latlng'+markeridx);
+        // // console.log(svg);
+        //     svg.append('circle').attr('r',5);
+        // var svg=d3.select('.routechart').select('svg');
+
 
         crimeInfoWindow.open(marker.get('map'), marker);
     });
-    marker.addListener('mouseout', function() {
-        // console.log(data[crimeidx]);
 
-        crimeInfoWindow.close();
-    });
+    // marker.addListener('mouseout', function() {
+    //     // console.log(data[crimeidx]);
+
+    //     crimeInfoWindow.close();
+    // });
 }
