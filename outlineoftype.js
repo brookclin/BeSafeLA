1fea22854c77a93161ee00ee70d4da65b2c3c22a
triggerlin <lc32402@gmail.com>
2017-11-27 23:39:35 -0800

    integrate treemap into 1st component

--- outlineoftype.js
+++ outlineoftype.js
@@ -1,80 +1,178 @@
-var w = 1120,
-    h = 600,
-    x = d3.scaleLinear().range([0, w]),
-    y = d3.scaleLinear().range([0, h]);
+function drawtree() {
+    // main svg
+    var svg = d3.select("div#v-pills-type").select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        g = svg.append("g").attr("transform", "translate(20,0)");       // move right 20px.
 
-var vis = d3.select("#v-pills-type").append("div")
-    .attr("class", "chart")
-    .style("width", w + "px")
-    .style("height", h + "px")
-    .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h);
+    // x-scale and x-axis
+    // var experienceName = ["", "Basic 1.0","Alright 2.0","Handy 3.0","Expert 4.0","Guru 5.0"];
+    // var formatSkillPoints = function (d) {
+    //     return experienceName[d % 6];
+    // }
+    var xScale = d3.scaleLinear()
+        .domain([10000, 20000])
+        .range([0, 350]);
 
-// var root1 = d3.hierarchy(data, getChildren)
-//     .sum(function(d) { return 1})
-//     .sort(null);
-var partition = d3.partition()
-    .size([h, w])
-    .padding(1)
-    .round(true);
+    var xAxis = d3.axisTop()
+        .scale(xScale)
+        .ticks(10)
+    // .tickFormat(formatSkillPoints);
 
-// .value(function(d) { return d.size; });
+    // Setting up a way to handle the data
+    var tree = d3.cluster()                 // This D3 API method setup the Dendrogram datum position.
+        .size([height, width - 500])    // Total width - bar chart width = Dendrogram chart width
+        .separation(function separate(a, b) {
+            return a.parent == b.parent            // 2 levels tree grouping for category
+                || a.parent.parent == b.parent
+                || a.parent == b.parent.parent ? 0.4 : 0.8;
+        });
 
-d3.json("yearandcode.json", function (data) {
-    
-    var root = d3.hierarchy(data);
-    partition(d3.hierarchy(data));
-    console.log(root);
+    var stratify = d3.stratify()            // This D3 API method gives cvs file flat data array dimensions.
+        .parentId(function (d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
 
-    var g = vis.selectAll("g")
-        .data(root.descendants())
-        .enter().append("svg:g")
-        .attr("transform", function (d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; })
-        .on("click", click);
+    d3.csv("layout.csv", row, function (error, data) {
+        if (error) throw error;
 
-    var kx = w / root.dx,
-        ky = h / 1;
+        var root = stratify(data);
+        tree(root);
 
-    g.append("svg:rect")
-        .attr("width", root.dy * kx)
-        .attr("height", function (d) { return d.dx * ky; })
-        .attr("class", function (d) { return d.children ? "parent" : "child"; });
+        // Draw every datum a line connecting to its parent.
+        var link = g.selectAll(".link")
+            .data(root.descendants().slice(1))
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", function (d) {
+                return "M" + d.y + "," + d.x
+                    + "C" + (d.parent.y + 100) + "," + d.x
+                    + " " + (d.parent.y + 100) + "," + d.parent.x
+                    + " " + d.parent.y + "," + d.parent.x;
+            });
 
-    g.append("svg:text")
-        .attr("transform", transform)
-        .attr("dy", ".35em")
-        .style("opacity", function (d) { return d.dx * ky > 12 ? 1 : 0; })
-        .style("font-size", "16px")
-        .text(function (d) { return d.name; })
+        // Setup position for every datum; Applying different css classes to parents and leafs.
+        var node = g.selectAll(".node")
+            .data(root.descendants())
+            .enter().append("g")
+            .attr("class", function (d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+            .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
 
-    d3.select(window)
-        .on("click", function () { click(root); })
+        // Draw every datum a small circle.
+        node.append("circle")
+            .attr("r", 4);
 
-    function click(d) {
-        if (!d.children) return;
+        // Setup G for every leaf datum.
+        var leafNodeG = g.selectAll(".node--leaf")
+            .append("g")
+            .attr("class", "node--leaf-g")
+            .attr("transform", "translate(" + 8 + "," + -13 + ")");
 
-        kx = (d.y ? w - 40 : w) / (1 - d.y);
-        ky = h / d.dx;
-        x.domain([d.y, 1]).range([d.y ? 40 : 0, w]);
-        y.domain([d.x, d.x + d.dx]);
+        leafNodeG.append("rect")
+            .attr("class", "shadow")
+            .style("fill", function (d) { return d.data.color; })
+            .attr("width", 2)
+            .attr("height", 20)
+            .attr("rx", 2)
+            .attr("ry", 2)
+            .transition()
+            .duration(800)
+            .attr("width", function (d) { return xScale(d.data.value); });
 
-        var t = g.transition()
-            .duration(d3.event.altKey ? 7500 : 750)
-            .attr("transform", function (d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; });
+        leafNodeG.append("text")
+            .attr("dy", 14.5)
+            .attr("x", 5)
+            .style("text-anchor", "start")
+            .style("font-size", "11px")
+            .text(function (d) {
+                return d.data.id.substring(d.data.id.lastIndexOf(".") + 1).split(" ")[0];
+            });
 
-        t.select("rect")
-            .attr("width", d.dy * kx)
-            .attr("height", function (d) { return d.dx * ky; });
+        // Write down text for every parent datum
+        var internalNode = g.selectAll(".node--internal");
+        internalNode.append("text")
+            .attr("y", -10)
+            .style("text-anchor", "middle")
+            .style("font-size", "11px")
+            .text(function (d) {
+                return d.data.id.substring(d.data.id.lastIndexOf(".") + 1);
+            });
 
-        t.select("text")
-            .attr("transform", transform)
-            .style("opacity", function (d) { return d.dx * ky > 12 ? 1 : 0; });
+        // Attach axis on top of the first leaf datum.
+        // var firstEndNode = g.select(".node--leaf");
+        //     firstEndNode.insert("g")
+        //             .attr("class","xAxis")
+        //             .attr("transform", "translate(" + 7 + "," + -14 + ")")
+        //             .call(xAxis);
 
-        d3.event.stopPropagation();
-    }
+        //     // tick mark for x-axis
+        //     firstEndNode.insert("g")
+        //             .attr("class", "grid")
+        //             .attr("transform", "translate(7," + (height - 15) + ")")
+        //             .call(d3.axisBottom()
+        //                     .scale(xScale)
+        //                     .ticks(5)
+        //                     .tickSize(-height, 0, 0)
+        //                     .tickFormat("")
+        //             );
 
-    function transform(d) {
-        return "translate(8," + d.dx * ky / 2 + ")";
-    }
-});
+        // Emphasize the y-axis baseline.
+        svg.selectAll(".grid").select("line")
+            .style("stroke-dasharray", "20,1")
+            .style("stroke", "black");
+
+        // The moving ball
+        var ballG = svg.insert("g")
+            .attr("class", "ballG")
+            .attr("transform", "translate(" + 1100 + "," + height / 2 + ")");
+        ballG.insert("circle")
+            .attr("class", "shadow")
+            .style("fill", "steelblue")
+            .attr("r", );
+        ballG.insert("text")
+            .style("text-anchor", "middle")
+            .style("font-size", "10px")
+            .attr("dy", 3);
+
+        // Animation functions for mouse on and off events.
+        d3.selectAll(".node--leaf-g")
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+
+        function handleMouseOver(d) {
+            var leafG = d3.select(this);
+
+            leafG.select("rect")
+                .attr("stroke", "#4D4D4D")
+                .attr("stroke-width", "2");
+
+
+            var ballGMovement = ballG.transition()
+                .duration(400)
+                .attr("transform", "translate(" + (d.y
+                    + xScale(d.data.value) + 90) + ","
+                + (d.x + 1.5) + ")");
+
+            ballGMovement.select("circle")
+                .style("fill", d.data.color)
+                .attr("r", 18);
+
+            ballGMovement.select("text")
+                .delay(300)
+                .text(Number(d.data.value).toFixed(0));
+        }
+        function handleMouseOut() {
+            var leafG = d3.select(this);
+
+            leafG.select("rect")
+                .attr("stroke-width", "0");
+        }
+
+    });
+}
+function row(d) {
+    return {
+        id: d.id,
+        value: +d.value,
+        color: d.color
+    };
+}
+drawtree();
